23e57221dcd46fa8e5a6a7aa016a3c94245e4ba8
Lucas Batista <lucashl8256@gmail.com>
2024-06-28 14:30:48 -0300

    criação do package de Repository, Service, configurações de application.properties

--- src/main/java/org/example/MinhaConfiguration.java
+++ src/main/java/org/example/MinhaConfiguration.java
@@ -1,6 +1,8 @@
 package org.example;
-import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguration {
+
 }
---
+++ src/main/java/org/example/service/ClientesService.java
@@ -0,0 +1,25 @@
+package org.example.service;
+
+import org.example.model.Cliente;
+import org.example.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientesService {
+
+    private ClientesRepository repository;
+
+    @Autowired
+    public ClientesService(ClientesRepository repository) {
+        this.repository = repository;
+    }
+    public void salvarCliente(Cliente cliente) {
+        valdiarCliente(cliente);
+        this.repository.persistir(cliente);
+    }
+
+    public void valdiarCliente(Cliente cliente) {
+        //aplica validações
+    }
+}
