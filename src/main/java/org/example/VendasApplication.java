72b771b89b78832d36d67ae726cb048a9e9cccec
Lucas Batista <lucashl8256@gmail.com>
2024-07-13 10:11:31 -0300

    Mapeamento de Entidades e criação dos Repositorys

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -18,31 +18,12 @@ public class VendasApplication {
         return args -> {
 
             System.out.println("Salvando Clientes");
-            clientes.salvar(new Cliente("Lucas"));
-            clientes.salvar(new Cliente("Dougllas"));
+            clientes.save(new Cliente("Lucas"));
+            clientes.save(new Cliente("Dougllas"));
 
-            List<Cliente> todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
+            List<Cliente> result = clientes.encontrarPorNome("as");
+            result.forEach(System.out::println);
 
-            System.out.println("Atulizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.atualizar(c);
-            });
-            System.out.println("Pesquisa por nome em especifico");
-            clientes.buscarPorNome("las").forEach(System.out::println);
-
-            clientes.obterTodos().forEach(c->{
-                clientes.deletar(c);
-            });
-
-            todosClientes =clientes.obterTodos();
-            if (todosClientes.isEmpty()) {
-                System.out.println("Não há clientes a serem exibidos.");
-            }else{
-                System.out.println("Exibindo todos clientes");
-                todosClientes.forEach(System.out::println);
-            }
         };
     }
     public static void main(String[] args) {
--- src/main/java/org/example/domain/entity/Produto.java
+++ src/main/java/org/example/domain/entity/Produto.java
@@ -1,11 +1,21 @@
 package org.example.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco_unitario", length = 20, precision = 2)
     private BigDecimal preco;
 
     public Integer getId() {
