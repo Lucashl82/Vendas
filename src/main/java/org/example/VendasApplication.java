9af56ccac68c00c68532a25288781728e19abe11
Lucas Batista <lucashl8256@gmail.com>
2024-07-13 13:07:31 -0300

    Criação de pedidos e metodos para consulta de Cliente mais pedido, correção de erro

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -1,28 +1,45 @@
 package org.example;
 
 import org.example.domain.entity.Cliente;
+import org.example.domain.entity.Pedido;
 import org.example.domain.repository.Clientes;
+import org.example.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes) {
+    public CommandLineRunner init(
+            @Autowired Clientes clientes,
+            @Autowired Pedidos pedidos
+    ) {
         return args -> {
 
             System.out.println("Salvando Clientes");
-            clientes.save(new Cliente("Lucas"));
-            clientes.save(new Cliente("Dougllas"));
+            Cliente fulano = new Cliente("Lucas");
+            clientes.save(fulano);
 
-            List<Cliente> result = clientes.encontrarPorNome("as");
-            result.forEach(System.out::println);
+            Pedido p = new Pedido();
+            p.setCliente(fulano);
+            p.setDataPedido( LocalDate.now() );
+            p.setTotal(BigDecimal.valueOf(100));
+
+            pedidos.save(p);
+
+            /*Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
+            System.out.println(cliente);
+            System.out.println(cliente.getPedidos());*/
+
+            pedidos.findByCliente(fulano).forEach(System.out::println);
 
         };
     }
--- src/main/java/org/example/domain/repository/Clientes.java
+++ src/main/java/org/example/domain/repository/Clientes.java
@@ -10,7 +10,13 @@ import java.util.List;
 
 public interface Clientes extends JpaRepository<Cliente, Integer > {
 
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ",nativeQuery = true)
     List<Cliente> encontrarPorNome(@Param("nome") String nome);
 
     boolean existsByNome(String nome);
+
+    @Query(" select c from Cliente c left join fetch c.pedidos where c.id = :id ")
+    Cliente findClienteFetchPedidos( @Param("id") Integer id );
+
+
 }
