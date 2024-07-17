c77b36b1b29530ab0e27f3308354c41e3bada747
Lucas Batista <lucashl8256@gmail.com>
2024-07-17 16:40:13 -0300

    Criação de rotas para salvar Clientes, e consultar clientes

---
+++ src/main/java/org/example/rest/controller/ClienteController.java
@@ -0,0 +1,37 @@
+package org.example.rest.controller;
+
+import org.example.domain.entity.Cliente;
+import org.example.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class ClienteController {
+
+
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity getClienteById( @PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity saveCliente ( @RequestBody Cliente cliente ){
+       Cliente clientesSalvo = clientes.save(cliente);
+       return ResponseEntity.ok(clientesSalvo);
+    }
+}
