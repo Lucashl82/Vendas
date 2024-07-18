9d21fa2fa9261b7ae090cfc7e1bffa22fc5649cb
Lucas Batista <lucashl8256@gmail.com>
2024-07-18 11:53:02 -0300

    Criação de novas rotas, de deleção, atualização e consultas

--- src/main/java/org/example/domain/entity/Cliente.java
+++ src/main/java/org/example/domain/entity/Cliente.java
@@ -16,6 +16,9 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+
     @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private Set<Pedido> pedidos;
@@ -42,6 +45,14 @@ public class Cliente {
 
     public Integer getId() {
         return id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     public void setId(Integer id) {
--- src/main/java/org/example/rest/controller/ClienteController.java
+++ src/main/java/org/example/rest/controller/ClienteController.java
@@ -2,10 +2,13 @@ package org.example.rest.controller;
 
 import org.example.domain.entity.Cliente;
 import org.example.domain.repository.Clientes;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -20,7 +23,7 @@ public class ClienteController {
 
     @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public ResponseEntity getClienteById( @PathVariable Integer id ){
+    public ResponseEntity getClienteById( @PathVariable Integer id ) {
         Optional<Cliente> cliente = clientes.findById(id);
         if(cliente.isPresent()){
             return ResponseEntity.ok(cliente.get());
@@ -30,8 +33,48 @@ public class ClienteController {
 
     @PostMapping("/api/clientes")
     @ResponseBody
-    public ResponseEntity saveCliente ( @RequestBody Cliente cliente ){
+    public ResponseEntity save ( @RequestBody Cliente cliente ) {
        Cliente clientesSalvo = clientes.save(cliente);
        return ResponseEntity.ok(clientesSalvo);
+    }
+
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete( @PathVariable Integer id ) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity update( @PathVariable Integer id,
+                                  @RequestBody Cliente cliente ) {
+        return clientes
+                .findById(id)
+                .map( clienteExistente-> {
+                    cliente.setId(clienteExistente.getId());
+                    clientes.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet( ()-> ResponseEntity.notFound().build() );
+    }
+
+    @GetMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity find( Cliente filtro ) {
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(
+                        ExampleMatcher.StringMatcher.CONTAINING );
+
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 }
