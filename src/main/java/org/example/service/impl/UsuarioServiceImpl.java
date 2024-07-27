faa44f2cf66d5840a18b12e3ea3d4e83a3a8fb9a
Lucas Batista <lucashl8256@gmail.com>
2024-07-27 11:43:43 -0300

    Criação da classe de usuarios para permitir buscar o login diretamente da base de dados

--- src/main/java/org/example/service/impl/UsuarioServiceImpl.java
+++ src/main/java/org/example/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,7 @@
 package org.example.service.impl;
 
+import org.example.domain.entity.Usuario;
+import org.example.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,23 +10,34 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class UsuarioServiceImpl implements UserDetailsService {
 
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return repository.save(usuario);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if(!username.equals("cicrano")){
-            throw new UsernameNotFoundException("Usuário não encontrado na base.");
-        }
+        Usuario usuario = repository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado na base de dados."));
 
+        String[] roles = usuario.isAdmin() ?
+                new String[]{"ADMIN", "USER"} : new String[]{"USER"};
         return User
                 .builder()
-                .username("cicrano")
-                .password(encoder.encode("123"))
-                .roles("USER", "ADMIN")
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(roles)
                 .build();
     }
 
